ac825e9b5f62512bd9e0df591cb79a5bea7a3456
JSCreation <purplelow1@gmail.com>
2022-01-14 14:13:04 +0900

    commit f

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,92 @@
+import ApexCharts from "react-apexcharts";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId: string;
+  isDark: boolean;
+}
+
+function Chart({ coinId, isDark }:ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <>
+      {isLoading ?
+        "Loading chart . . . "
+        : (
+          <ApexCharts
+            type="line"
+            series={[
+              {
+                name: "Price",
+                data: data?.map((price) => price.close),
+              },
+            ]}
+            options={{
+              theme: {
+                mode: isDark ? "dark" : "light",
+              },
+              chart: {
+                height: 300,
+                width: 500,
+                toolbar: {
+                  show: false,
+                },
+                background: "tranparent"
+              },
+              grid: {
+                show: false,
+              },
+              stroke: {
+                curve: "smooth",
+              },
+              yaxis: {
+                show: false,
+              },
+              xaxis: {
+                axisBorder: {
+                  show: false,
+                },
+                axisTicks: {
+                  show: false,
+                },
+                labels: {
+                  show: false,
+                },
+                type: "datetime",
+                categories: data?.map((price) => price.time_close),
+              },
+              fill: {
+                type: "gradient",
+                gradient: {
+                  gradientToColors: ["#2ecc71"],
+                  stops: [0, 100]
+                },
+              },
+              colors: ["#3498db"],
+              tooltip: {
+                y: {
+                  formatter: (value) => `$ ${value.toFixed(2)}`,
+                },
+              }
+            }}
+          />
+        )}
+    </>
+  );
+}
+
+export default Chart;
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,124 @@
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { fetchCoins } from "../api";
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto; 
+`;
+
+const Header = styled.header`
+  display: flex;
+  height: 10vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinList = styled.ul`
+  
+`;
+
+const Coin = styled.li`
+  background-color: ${(props) => props.theme.cardBgColor};
+  color: ${(porps) => porps.theme.textColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  border: 1px solid #fff;
+  transition: all .01s ease-in;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.5s ease-in;
+  }
+  &:hover{
+    background-color: #2ecc71;
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 36px;
+  color: ${(props) => props.theme.accentColor}; 
+`;
+
+const Loader = styled.span`
+  display: block;
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface ICoin {
+  id: string,
+  name: string,
+  symbol: string,
+  rank: number,
+  is_new: boolean,
+  is_active: boolean,
+  type: string,
+}
+
+interface ICoinsProps {
+  toggleDark: () => void;
+}
+
+function Coins({ toggleDark }: ICoinsProps) {
+  const { isLoading, data } = useQuery<ICoin[]>("allcoins", fetchCoins);
+  // const [coins, setCoins] = useState<CoinInterface[]>([]);
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await fetch("https://api.coinpaprika.com/v1/coins");
+  //     const json = await response.json();
+  //     setCoins(json.slice(0, 100));
+  //     // setTimeout(() => { setLoading(false) }, 300);
+  //     setLoading(false);
+  //   })();
+  // }, []);
+
+  return (
+    <Container>
+      <Helmet>
+        <title>
+          All Coins
+        </title>
+      </Helmet>
+      <Header>
+        <Title>All Coins</Title>
+        <button onClick={toggleDark}>Toggle Mode</button>
+      </Header>
+      {isLoading ? (
+        <Loader>Loading . . . </Loader>
+      ) : (
+        <CoinList>
+        {data?.slice(0, 100).map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={{
+              pathname: `/${coin.id}`,
+              state: {
+                name: coin.name
+              },
+            }}>
+              <Img src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+              {coin.name} &rarr;
+            </Link>
+          </Coin>
+        ))}
+        </CoinList>
+      )}
+    </Container>
+  )
+};
+
+export default Coins;
